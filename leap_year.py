93ef9c434c16d7bda04cb41f24356315223f71ec
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-04-13 12:38:19 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,5 +4,26 @@
 
 # Don't forget to reach out for help after your own due diligence
 
-def is_leap_year(year):
-    return ''
+def is_leap(year):
+    # your code here
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+        
+       
+
+# Below is a set of tests so you can check if your code is correct.
+from test import testEqual
+
+testEqual(is_leap(1944), True)
+testEqual(is_leap(2011), False)
+testEqual(is_leap(1986), False)
+testEqual(is_leap(1956), True)
+testEqual(is_leap(1957), False)
+testEqual(is_leap(1800), False)
+testEqual(is_leap(1900), False)
+testEqual(is_leap(1600), True)
+testEqual(is_leap(2056), True)
