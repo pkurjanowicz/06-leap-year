9e4820eb2110f275fbb4a4e2015137418b0961a4
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-04-14 18:50:31 -0600

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,12 +4,11 @@
 
 # Don't forget to reach out for help after your own due diligence
 
-def is_leap(year):
+def is_leap_year(year):
     if year % 4 == 0 and year % 100 != 0:
         return True
     elif year % 400 != 0:
         return False
     else:
         return True
-        
        
